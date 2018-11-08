ceea11dd04a1e12de8688e38425006d801ea819f
togoshigekata <togoshigekata@gmail.com>
2018-11-07 18:53:29 -0600

    Commented in full crontab

--- explorer-index-resetter-togo.sh
+++ explorer-index-resetter-togo.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+##############
 # Setup: 
+#
 # crontab -e
 # */4 * * * * /home/explorer-index-resetter-togo.sh > /dev/null 2>&1
+#
+# Full Crontab with BiblePay Daemon and Mongo Service Checking:
+#
+# */2 * * * * cd /home/explorer && /usr/bin/nodejs --stack-size=15000 scripts/sync.js index update > /dev/null 2>&1
+# */6 * * * * cd /home/explorer && /usr/bin/nodejs scripts/sync.js market > /dev/null 2>&1
+# */11 * * * * cd /home/explorer && /usr/bin/nodejs scripts/peers.js > /dev/null 2>&1
+#
+# */5 * * * * /home/biblepay/src/biblepayd > /dev/null 2>&1
+# */4 * * * * /home/explorer-index-resetter-togo.sh > /dev/null 2>&1
+# 0 */3 * * * /usr/sbin/service mongod start > /dev/null 2>&1
+##############
 
 fname="/home/explorer/tmp/index.pid"
 
